c87a1779b66b3ef62c386adb09da69f35bfd6829
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-09-11 13:12:12 +0530

    using fs-extra instead of fsPromises

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const fsPromises = require("fs").promises;
 const path = require("path");
 
-const txt_Files = path.join(__dirname, "/text");
+const fsx = require("fs-extra");
+
+const txt_Files = path.join(__dirname, "text"); //directory name
+
+async () => {
+    try {
+        await fsx.ensureDir(txt_Files);
+        console.log("File exists / File Created");
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 const base_name = 000;
 const file_count = 46; // number of files you want to create
@@ -10,11 +20,11 @@ let start_index = 0;
 const createTxtFiles = async () => {
     for (i = 0; i <= file_count; i++) {
         start_index = base_name + i;
-        await fsPromises
-            .writeFile(`${txt_Files}/${start_index}.txt`, "")
-            .catch((err) => {
-                console.log(err);
-            });
+        try {
+            await fsx.createFile(`${txt_Files}/${start_index}.txt`)
+        } catch (err) {
+            console.log(err);
+        }
     }
     console.log("Loop ended");
 };
