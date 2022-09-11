bd4741b9eb333c31c0f2c9b5caafbbdd09a1f997
Hejo <59996938+DevHejo@users.noreply.github.com>
2022-09-11 13:17:15 +0530

    fixed a condition to check if directory exists

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const path = require("path");
 const fsx = require("fs-extra");
 
 const txt_Files = path.join(__dirname, "text"); //directory name
+const base_name = 000;
+const file_count = 46; // number of files you want to create
+let start_index = 0;
 
-async () => {
+const createTxtFiles = async () => {
+    // check if directory exists
     try {
         await fsx.ensureDir(txt_Files);
         console.log("File exists / File Created");
     } catch (err) {
         console.log(err);
     }
-};
-
-const base_name = 000;
-const file_count = 46; // number of files you want to create
-let start_index = 0;
-
-const createTxtFiles = async () => {
+    // make text files
     for (i = 0; i <= file_count; i++) {
         start_index = base_name + i;
         try {
